0bfc92008307d78b70976464929314c64249c5e1
Ruin800 <uyusuususjajxjxjiss@gmail.com>
2026-04-01 14:18:48 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config(); // Load .env variables
+const express = require("express");
+const app = express();
+
+// GET endpoint returning text
+app.get("/", (req, res) => {
+  const message = process.env.TEXT_MESSAGE || "Default text response!";
+  res.send(message);
+});
+
+// Another GET endpoint example
+app.get("/info", (req, res) => {
+  res.send("This is a secondary endpoint.");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
